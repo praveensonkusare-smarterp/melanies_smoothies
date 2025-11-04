fe5ce3c2051d9d138e1eddec64d595e923c25da2
praveensonkusare-smarterp <praveen.sonkusare@smarterp.com>
2025-11-04 10:47:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,16 @@ if ingredients_list and name_on_order:
             st.success("Your Smoothie is ordered!", icon="✅")
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# # st.text(smoothiefroot_response.json())
+# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
+if ingredients_list:
+	ingredients_string =''
+	for fruit_chosen in ingredients_list:
+		ingredients_string += fruit_chosen + ''
+		smoothiefroot_response = requests.get("https://my.smothiefroot.com/api/fruit/watermelon")
+		sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
 
         
