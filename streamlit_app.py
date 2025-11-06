ecc0118e84ab68c88fd1e549af2b0607fb9d2e3a
praveensonkusare-smarterp <praveen.sonkusare@smarterp.com>
2025-11-06 10:36:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,12 @@ session = cnx.session()
 # Fetch fruit options
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col('SEARCH_ON'))
 # fruit_list = [row["FRUIT_NAME"] for row in my_dataframe.collect()]
-st.dataframe(data=my_dataframe, use_container_width = True)
+# st.dataframe(data=my_dataframe, use_container_width = True)
 
+# st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 # Multi-select input
